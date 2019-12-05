9659a320e88c80ea6afd9cf824644e045f10dd6a
Ronny <ronny.rook@labela.nl>
2019-12-05 16:03:33 +0100

    Change Container for tablet

--- src/components/layout/Container/index.js
+++ src/components/layout/Container/index.js
@@ -5,9 +5,13 @@ const Container = styled.div`
   position: relative;
   width: 100%;
 
+  ${media.tablet`
+    max-width: 960px;
+    margin: 0 auto;
+  `}
+
   ${media.desktop`
     max-width: 1200px;
-    margin: 0 auto;
   `}
 `;
 
