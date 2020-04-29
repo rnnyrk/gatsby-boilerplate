300cc8be1401c892205a8b57245ec385c982e0ef
Ronny Rook <info@ronnyrook.nl>
2020-04-29 21:26:32 +0200

    Re-enable all plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,57 +14,57 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
     'gatsby-plugin-typescript',
-    // 'gatsby-image',
-    // 'gatsby-plugin-sharp',
-    // 'gatsby-transformer-sharp',
-    // {
-    //   resolve: 'gatsby-source-filesystem',
-    //   options: {
-    //     name: 'images',
-    //     path: `${__dirname}/src/assets/images/`,
-    //   },
-    // },
-    // {
-    //   resolve: 'gatsby-plugin-module-resolver',
-    //   options: {
-    //     root: './src',
-    //     aliases: {
-    //       components: './components',
-    //       common: './components/common',
-    //       modules: './components/modules',
-    //       pages: './pages',
-    //       styles: './assets/styles',
-    //       services: './services',
-    //       templates: './templates',
-    //       images: './assets/images',
-    //       vectors: './assets/vectors',
-    //       static: {
-    //         root: './public',
-    //         alias: './static',
-    //       },
-    //     },
-    //   },
-    // },
-    // {
-    //   resolve: 'gatsby-plugin-react-svg',
-    //   options: {
-    //     rule: {
-    //       include: `${__dirname}/src/assets/vectors/internal/`,
-    //     },
-    //   },
-    // },
-    // {
-    //   resolve: 'gatsby-plugin-manifest',
-    //   options: {
-    //     name: 'gatsby-boilerplate-ts',
-    //     short_name: 'starter',
-    //     start_url: '/',
-    //     background_color: '#010101',
-    //     theme_color: '#e7ff04',
-    //     display: 'minimal-ui',
-    //     icon: 'src/assets/images/prime-icon.png',
-    //   },
-    // },
-    // 'gatsby-plugin-offline',
+    'gatsby-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/assets/images/`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-module-resolver',
+      options: {
+        root: './src',
+        aliases: {
+          components: './components',
+          common: './components/common',
+          modules: './components/modules',
+          pages: './pages',
+          styles: './assets/styles',
+          services: './services',
+          templates: './templates',
+          images: './assets/images',
+          vectors: './assets/vectors',
+          static: {
+            root: './public',
+            alias: './static',
+          },
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: `${__dirname}/src/assets/vectors/internal/`,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-boilerplate-ts',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#010101',
+        theme_color: '#e7ff04',
+        display: 'minimal-ui',
+        icon: 'src/assets/images/prime-icon.png',
+      },
+    },
+    'gatsby-plugin-offline',
   ],
 };
